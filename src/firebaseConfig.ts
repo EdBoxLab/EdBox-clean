a2f4be291f08336b1be86a2907be0ac43b271ced
edbox01101 <edbox01101@gmail.com>
2025-11-21 21:18:20 +0000

    using only firebase for auth

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,6 @@
 
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZDpY3UxOBJsYGJ0DtVnoKuImQy5p9l2A",
@@ -15,4 +14,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
-export const db = getFirestore(app);
